908a35b9f386141d50c6ae0d5b92900cbfa685aa
sameh lancaster <sam4420507@hotmail.com>
2020-03-17 11:26:51 +0000

    Add project files.

---
+++ DutchTreat/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using AutoMapper;
+using DutchTreat.Data;
+using DutchTreat.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DutchTreat
+{
+  public class Startup
+  {
+    private readonly IConfiguration _config;
+
+    public Startup(IConfiguration config)
+    {
+      _config = config;
+    }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<DutchContext>(cfg =>
+      {
+        cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString"));
+      });
+
+      services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+      services.AddTransient<DutchSeeder>();
+
+      services.AddScoped<IDutchRepository, DutchRepository>();
+
+      services.AddTransient<IMailService, NullMailService>();
+      // Support for real mail service
+
+      services.AddControllersWithViews();
+
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseStaticFiles();
+      app.UseNodeModules();
+
+      app.UseRouting();
+
+      app.UseEndpoints(cfg =>
+      {
+        cfg.MapControllerRoute("Default",
+              "{controller}/{action}/{id?}",
+              new { controller = "App", Action = "Index" });
+      });
+
+    }
+  }
+}
